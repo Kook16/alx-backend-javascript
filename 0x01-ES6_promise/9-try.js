6e30bb618d5f7429f8934f2969c5845936a759e1
kok <kok@ubuntu.ubuntu-domain>
2024-06-26 07:17:39 -0700

    added project tasks

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,12 @@
+export default function guardrail(mathFunction) {
+  let queue = [];
+  try {
+    queue.push(mathFunction());
+  }
+  catch(err) {
+    queue.push(`Error: ${err.message}`);
+  } finally {
+    queue.push('Guardrail was processed');
+  }
+  return queue;
+}
