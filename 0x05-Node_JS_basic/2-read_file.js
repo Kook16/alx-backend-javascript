ae839a0fe8447cb4701c767387ea19a695a02997
kok <kookcalvinayen@gmail.com>
2024-08-19 20:06:04 +0300

    used eslint format on ask 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 function countStudents(path) {
   try {
     const data = fs.readFileSync(path, 'utf8').split('\n');
-    const students = data.filter((line) => line.trim() !== '').slice(1);
+    const students = data.filter((line) => line).slice(1);
     const fields = {};
 
     students.forEach((student) => {
@@ -16,16 +16,16 @@ function countStudents(path) {
     });
 
     console.log(`Number of students: ${students.length}`);
-    for (const field in Object.keys(fields)) {
+    for (const field in fields) {
       console.log(
-        `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`
+        `Number of students in ${field}: ${
+          fields[field].length
+        }. List: ${fields[field].join(', ')}`
       );
     }
   } catch (error) {
-    console.error('Cannot load the database');
-    throw error;
+    throw new Error('Cannot load the database');
   }
 }
 
 module.exports = countStudents;
-
